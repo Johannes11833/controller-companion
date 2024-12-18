fdfeba80e8b706b6661e31521b0334410d7b2ce3
Johannes Gundlach <24914225+Johannes11833@users.noreply.github.com>
2024-12-18 20:18:08 +0100

    Add support for mac and linux for the kill by name action

--- controller_companion/app/utils.py
+++ controller_companion/app/utils.py
@@ -1,3 +1,5 @@
+from enum import Enum
+import platform
 from tkinter import Tk
 from PIL import Image, ImageTk
 
@@ -8,3 +10,13 @@ def set_window_icon(root: Tk):
     im = Image.open(resources.APP_ICON_ICO)
     photo = ImageTk.PhotoImage(im)
     root.wm_iconphoto(True, photo)
+
+
+class OperatingSystem(Enum):
+    WINDOWS = "Windows"
+    MAC = "Darwin"
+    LINUX = "Linux"
+
+
+def get_os() -> OperatingSystem:
+    return OperatingSystem(platform.system())
